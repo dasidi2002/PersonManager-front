01cda8c941f2d6c950586386de59193098e2d361
Daniel Silva <81590291+dasidi2002@users.noreply.github.com>
2025-03-20 14:31:41 -0300

    fix: lazing loading

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,11 @@
 import { Routes } from '@angular/router';
-import { CadastroPessoasComponent } from './components/cadastro-pessoas/cadastro-pessoas.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'cadastro-pessoas', pathMatch: 'full' },
-  { path: 'cadastro-pessoas', component: CadastroPessoasComponent },
-  { path: '**', redirectTo: 'cadastro-pessoas' } // Rota coringa para redirecionar para o cadastro de pessoas
+  {
+    path: 'cadastro-pessoas',
+    loadComponent: () => import('./components/cadastro-pessoas/cadastro-pessoas.component')
+      .then(c => c.CadastroPessoasComponent)
+  },
+  { path: '**', redirectTo: 'cadastro-pessoas' }
 ];
